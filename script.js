99273ac9f3d430a408476a746faca856ff4736d0
tanishk15sharma <tanishk1582002@gmail.com>
2021-12-06 10:29:25 +0530

    fix

--- script.js
+++ script.js
@@ -1,11 +1,41 @@
-const slider = document.querySelector(".items")
+const slider = document.querySelector(".items");
 let isDown = false;
 let startX;
 let scrollLeft;
 
 
-slider
+slider.addEventListener("mousedown",(e)=>{
+    isDown = true;
+    slider.classList.add("active");
+    startX = e.pageX - slider.offsetLeft ;
+    scrollLeft = slider.scrollLeft ;
+    // console.log(startX);
+}   )
+slider.addEventListener("mouseleave",()=>{
+    isDown = false;
+    slider.classList.remove("active");
+} )
+slider.addEventListener("mouseup",()=>{
+    isDown = false;
+    slider.classList.remove("active");
+    
+} )
+slider.addEventListener("mousemove",(e)=>{
+    if(!isDown) return 
+    //this will stop the function here only 
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft ;
+    // console.log({x , startX});
+    const walk = (x - startX) * 3 ;
+    // console.log(walk);
+    slider.scrollLeft =  scrollLeft - walk;
+ } )
 
 
 
-console.log(slider);
+
+
+
+
+
+
